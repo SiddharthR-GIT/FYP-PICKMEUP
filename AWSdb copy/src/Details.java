399c6c587051013aeb51805da6307d130210786a
SidCodeBanger <32364039+SidCodeBanger@users.noreply.github.com>
2018-01-31 17:32:07 +0000

    Working ith a new sign up page and vaildating an email

--- AWSdb copy/src/Details.java
+++ AWSdb copy/src/Details.java
@@ -4,9 +4,10 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,57 +21,38 @@ public class Details extends HttpServlet {
     private Connection connection;
     private Statement statement;
     private PreparedStatement find;
+    private boolean isEmailViable;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         response.setContentType("text/html");
         PrintWriter pr = response.getWriter();
 
+
         try{
 
-
-            /*String driver = "org.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://192.168.0.80/aws-database.czetjngd8wtj.eu-west-1.rds.amazonaws.com:3306/";
-            connection = DriverManager.getConnection(url+"Data?useSSL=false","MasterUser", "password");
-            Class.forName(driver);
-            find = connection.prepareStatement("SELECT *" + "FROM SignUp");
-            statement = connection.createStatement();*/
-
             String driver = "com.mysql.jdbc.Driver";
-            //
-            // String url = "jdbc:mysql://localhost:3306/SignUp?autoReconnect=true&useSSL=false";
-
             String url = "jdbc:mysql://rds-mysql-db.czetjngd8wtj.eu-west-1.rds.amazonaws.com:3306/Details?autoReconnect=true&useSSL=false";
             Class.forName(driver);  // load the driver
             connection = DriverManager.getConnection(url,"MasterUser","password");
 
             find = connection.prepareStatement("SELECT *" + "FROM peopleDetails");
             statement = connection.createStatement();
+            isEmailViable = isValidEmailAddress(request.getParameter("Email"));
 
-            String sqlStatement = "INSERT INTO peopleDetails (First_Name, Last_Name, Email, Title) " +
-                    "VALUES("
-                    +"'" + request.getParameter("first_name") +"'" +','
-                    +"'" + request.getParameter("last_name")+"'" +','
-                    +"'" + request.getParameter("Email") +"'"+ ','
-                    +"'" + request.getParameter("Title") +"')";
+            if(isEmailViable == true) {
+                String sqlStatement = "INSERT INTO peopleDetails (First_Name, Last_Name, Email, Title) " +
+                        "VALUES("
+                        +"'" + request.getParameter("first_name") +"'" +','
+                        +"'" + request.getParameter("last_name")+"'" +','
+                        +"'" + request.getParameter("Email") +"'"+ ','
+                        +"'" + request.getParameter("Title") +"')";
 
-            statement.executeUpdate(sqlStatement);
-
-            ResultSet resultSet = find.executeQuery();
-
-            pr.println("<html><head><title>Results</title></head><body>");
-            pr.println("<p> Values have been entered into Database");
-
-            while (resultSet.next()) {
-                pr.println("<p>Succes - ID Number:"+resultSet.getString(1)+" </p>");
-                pr.println();
-                pr.println("<p>" + resultSet.getString(1)
-                        + ", " + resultSet.getString("first_name")
-                        + ", " + resultSet.getString("last_name")
-                        + ", " + resultSet.getString("Email")
-                        + ", " + resultSet.getString("Title")
-                        + "</p>");
+                statement.executeUpdate(sqlStatement);
             }
-
+            else{
+                pr.println("<html><head><title>ERROR</title></head><body>");
+                pr.println("<p>You Problem is that the Email is not valid </p>");
+            }
         }catch(SQLException sql){
             sql.printStackTrace();
             pr.println("<html><head><title>ERROR</title></head><body>");
@@ -93,5 +75,18 @@ public class Details extends HttpServlet {
             }
 
         }
+    }
+    public static boolean isValidEmailAddress(String email){
+        boolean feedback = true;
+
+        try{
+            InternetAddress emailAdd = new InternetAddress(email);
+            emailAdd.validate(); // valid the email if its real or fake
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+            feedback = false;
+        }
+        return feedback;
     }
 }
