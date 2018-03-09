7befa464f5a8f5e9ff9b7938df6378be0e41fc30
SidCodeBanger <32364039+SidCodeBanger@users.noreply.github.com>
2018-03-09 12:26:03 +0000

    Hashing password using SHA256

---
+++ Hash_Password/src/stringHasing.java
@@ -0,0 +1,30 @@
+import java.security.MessageDigest;
+
+public class stringHasing {
+
+    public static void main(String[] args) {
+
+        String password = "Password";
+        String hp = sha_256(password);
+        System.out.println("Actual password - "+password);
+        System.out.println("Hash Password - "+hp);
+
+    }
+    public static String sha_256(String password){
+
+        try{
+            MessageDigest msg = MessageDigest.getInstance("SHA-256");
+            byte[] hash = msg.digest(password.getBytes("UTF-8"));
+            StringBuffer hexStr = new StringBuffer();
+
+            for(int i = 0; i< hash.length;i++){
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)hexStr.append('0');
+                    hexStr.append(hex);
+            }
+            return hexStr.toString();
+        }catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+}
