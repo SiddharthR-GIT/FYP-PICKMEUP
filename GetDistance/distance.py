4d4968d6cf0fc7ccb90a3d282c32ff549bc77dd0
SidCodeBanger <32364039+SidCodeBanger@users.noreply.github.com>
2018-04-06 18:11:21 +0100

    Minor Changes, Not used anymore though

--- GetDistance/distance.py
+++ GetDistance/distance.py
@@ -1,16 +1,14 @@
 import googlemaps
 import json
+import sys
 
-
-def distanceCalcaultor(origin, destination):
+def distanceCalcaultor(sys.argv[0], sys.argv[1]):
     f = open('distance.json','w')
     maps = googlemaps.Client('AIzaSyCFreEK4Ur8T7aV3CRG7pwSbKvfaT89YpQ')
 
-    my_distance = maps.distance_matrix(str(origin), str(destination))
+    my_distance = maps.distance_matrix(str(sys.argv[0]), str(sys.argv[1]))
     d = (str(my_distance))
     d2 = json.dumps(d)
     f.write(str(d2))
     f.close()
 
-
-distanceCalcaultor('Galway','Dublin')
